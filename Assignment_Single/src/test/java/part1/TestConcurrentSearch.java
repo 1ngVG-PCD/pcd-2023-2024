ffbf808d7d7ef4f813dc27b5453b03ba04ba1402
Valerio-Giannini <valerio.giannini@studio.unibo.it>
2025-01-28 11:17:28 +0100

    Testing SearchMethods: SeqSearch, ConcurrentSearch, VirtualThreadSearch

--- Assignment_Single/src/test/java/part1/TestConcurrentSearch.java
+++ Assignment_Single/src/test/java/part1/TestConcurrentSearch.java
@@ -2,6 +2,7 @@ package part1;
 
 import org.junit.jupiter.api.Test;
 import part1.src.step00.SeqSearch;
+import part1.src.step01.ConcurrentSearch;
 
 import java.io.File;
 import java.util.List;
@@ -9,27 +10,28 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static part1.src.services.ScanDirectory.getPdfFiles;
 
-public class TestSeqSearch {
+public class TestConcurrentSearch {
 
     File directoryPath = new File("C:\\Users\\giann\\Desktop\\PCD\\pcd-2023-2024\\Assignment_Single\\src\\main\\java\\part1\\TestSet");
     List<File> pdfs = getPdfFiles(directoryPath);
-    SeqSearch seqSearch = new SeqSearch();
+    ConcurrentSearch concurrentSearch = new ConcurrentSearch();
 
     @Test
     void TestAllFileHaveWord() {
         String word = "!";
-        assertEquals(301, seqSearch.run(pdfs, word));
+        assertEquals(301, concurrentSearch.run(pdfs, word));
     }
 
     @Test
     void TestNone() {
         String word = "wow";
-        assertEquals(0, seqSearch.run(pdfs, word));
+        assertEquals(0, concurrentSearch.run(pdfs, word));
     }
 
     @Test
     void TestMixedDirectory() {
         String word = "parola";
-        assertEquals(151, seqSearch.run(pdfs, word));
+        assertEquals(151, concurrentSearch.run(pdfs, word));
     }
 }
+
--- Assignment_Single/src/test/java/part1/TestVirtualThreadSearch.java
+++ Assignment_Single/src/test/java/part1/TestVirtualThreadSearch.java
@@ -1,7 +1,8 @@
 package part1;
 
 import org.junit.jupiter.api.Test;
-import part1.src.step00.SeqSearch;
+import part1.src.step01.ConcurrentSearch;
+import part1.src.step02.VirtualThreadSearch;
 
 import java.io.File;
 import java.util.List;
@@ -9,27 +10,27 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static part1.src.services.ScanDirectory.getPdfFiles;
 
-public class TestSeqSearch {
+public class TestVirtualThreadSearch {
 
     File directoryPath = new File("C:\\Users\\giann\\Desktop\\PCD\\pcd-2023-2024\\Assignment_Single\\src\\main\\java\\part1\\TestSet");
     List<File> pdfs = getPdfFiles(directoryPath);
-    SeqSearch seqSearch = new SeqSearch();
+    VirtualThreadSearch search = new VirtualThreadSearch();
 
     @Test
-    void TestAllFileHaveWord() {
+    void TestAllFileHaveWord() throws InterruptedException {
         String word = "!";
-        assertEquals(301, seqSearch.run(pdfs, word));
+        assertEquals(301, search.run(pdfs, word));
     }
 
     @Test
-    void TestNone() {
+    void TestNone() throws InterruptedException {
         String word = "wow";
-        assertEquals(0, seqSearch.run(pdfs, word));
+        assertEquals(0, search.run(pdfs, word));
     }
 
     @Test
-    void TestMixedDirectory() {
+    void TestMixedDirectory() throws InterruptedException {
         String word = "parola";
-        assertEquals(151, seqSearch.run(pdfs, word));
+        assertEquals(151, search.run(pdfs, word));
     }
 }
